3c0e52cd5b58049db462f8e236c5127cfc6b8b94
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-08-14 16:39:59 -0500

    app cleaned up

---
+++ client/components/forms/AuthForm.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { SyncOutlined } from "@ant-design/icons";
+const AuthForm = ({
+  handleSubmit,
+  name,
+  setName,
+  email,
+  setEmail,
+  password,
+  setPassword,
+  secret,
+  setSecret,
+  loading,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group p-2">
+        <small>
+          <label htmlFor="" className="text-muted">
+            Name:
+          </label>
+        </small>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          className="form-control"
+          placeholder="Enter name"
+        />
+      </div>
+      <div className="form-group p-2">
+        <small>
+          <label htmlFor="" className="text-muted">
+            Email:
+          </label>
+        </small>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+          placeholder="Enter email"
+        />
+      </div>
+      <div className="form-group p-2">
+        <small>
+          <label htmlFor="" className="text-muted">
+            Password:
+          </label>
+        </small>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
+          placeholder="Enter password"
+        />
+      </div>
+      <div className="form-group p-2">
+        <small>
+          <label htmlFor="" className="text-muted">
+            Pick a security question:
+          </label>
+        </small>
+        <select className="form-control">
+          <option>In what city were you born?</option>
+          <option>What is the name of your favorite pet?</option>
+          <option>What is your mother's maiden name?</option>
+          <option>What high school did you attend?</option>
+          <option>What was the name of your elementary school?</option>
+          <option>What was the make of your first car?</option>
+        </select>
+
+        <small className="form-text text-muted">
+          You can use this to reset your password!
+        </small>
+      </div>
+      <div className="form-group p-2">
+        <input
+          type="text"
+          value={secret}
+          onChange={(e) => setSecret(e.target.value)}
+          className="form-control"
+          placeholder="Security question's answer"
+        />
+      </div>
+      <div className="form-group p-2">
+        <button
+          disabled={!name || !email || !secret || !password}
+          className="btn btn-primary btn-block col-12"
+        >
+          {loading ? <SyncOutlined spin className="py-1" /> : "Submit"}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AuthForm;
--- client/pages/register.jsx
+++ client/pages/register.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { Modal } from "antd";
+import Link from "next/link";
+
+import AuthForm from "../components/forms/AuthForm";
+
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [secret, setSecret] = useState("");
   const [ok, setOk] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   // const data = {
   //   name,
   //   email,
@@ -17,13 +24,22 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post(`http://localhost:8000/api/register`, {
-        name,
-        email,
-        password,
-        secret,
-      });
+      setLoading(true);
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_API}/register`,
+        {
+          name,
+          email,
+          password,
+          secret,
+        }
+      );
       setOk(data.ok);
+      setName("");
+      setEmail("");
+      setPassword("");
+      setSecret("");
+      setLoading(false);
     } catch (error) {
       toast.error(error.response.data, {
         position: "top-center",
@@ -35,12 +51,9 @@ function Register() {
         progress: undefined,
         theme: "dark",
       });
+      setLoading(false);
     }
     // console.log(data
-    setName("");
-    setEmail("");
-    setPassword("");
-    setSecret("");
   };
 
   return (
@@ -52,85 +65,36 @@ function Register() {
           </h1>
         </div>
       </div>
+      {/* {loading ? <h1>Loading</h1> : ""} */}
       <div className="row py-4">
         <div className="col-md-6 offset-md-3">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group p-2">
-              <small>
-                <label htmlFor="" className="text-muted">
-                  Name:
-                </label>
-              </small>
-              <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                type="text"
-                className="form-control"
-                placeholder="Enter name"
-              />
-            </div>
-            <div className="form-group p-2">
-              <small>
-                <label htmlFor="" className="text-muted">
-                  Email:
-                </label>
-              </small>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="form-control"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="form-group p-2">
-              <small>
-                <label htmlFor="" className="text-muted">
-                  Password:
-                </label>
-              </small>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="form-control"
-                placeholder="Enter password"
-              />
-            </div>
-            <div className="form-group p-2">
-              <small>
-                <label htmlFor="" className="text-muted">
-                  Pick a security question:
-                </label>
-              </small>
-              <select className="form-control">
-                <option>In what city were you born?</option>
-                <option>What is the name of your favorite pet?</option>
-                <option>What is your mother's maiden name?</option>
-                <option>What high school did you attend?</option>
-                <option>What was the name of your elementary school?</option>
-                <option>What was the make of your first car?</option>
-              </select>
-
-              <small className="form-text text-muted">
-                You can use this to reset your password!
-              </small>
-            </div>
-            <div className="form-group p-2">
-              <input
-                type="text"
-                value={secret}
-                onChange={(e) => setSecret(e.target.value)}
-                className="form-control"
-                placeholder="Security question's answer"
-              />
-            </div>
-            <div className="form-group p-2">
-              <button className="btn btn-primary btn-block col-12">
-                Submit
-              </button>
-            </div>
-          </form>
+          <AuthForm
+            handleSubmit={handleSubmit}
+            name={name}
+            setName={setName}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            secret={secret}
+            setSecret={setSecret}
+            loading={loading}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <Modal
+            title="Congrates"
+            visible={ok}
+            onCancel={() => setOk(false)}
+            footer={null}
+          >
+            <p>you have seccussflly registered</p>
+            <Link href="/login">
+              <a className="btn btn-primary">Login Now</a>
+            </Link>
+          </Modal>
         </div>
       </div>
     </div>
