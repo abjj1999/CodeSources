b4625ff71a212d85126e01b38f6404b935c23b72
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-08-16 22:25:51 -0500

    FE User Protected routes are in place

--- client/pages/user/dashboard.jsx
+++ client/pages/user/dashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import UserRoutes from "../../components/routes/UserRoutes";
 const Dashboard = () => {
   return (
-    <div className="row">
-      <div className="col">
-        <h1 className="display-1 text-center">DashBoard Page</h1>
+    <UserRoutes>
+      <div className="row">
+        <div className="col">
+          <h1 className="display-1 text-center">DashBoard Page</h1>
+        </div>
       </div>
-    </div>
+    </UserRoutes>
   );
 };
 
