24b03525dea779da8a3b6f2719112a04392bfdda
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-08-20 18:22:33 -0500

    downloded ReactQuill for the post text area using Next/dynamic

--- client/components/forms/CreatePostForm.jsx
+++ client/components/forms/CreatePostForm.jsx
@@ -1,22 +1,34 @@
 import React from "react";
+//some packages are not supported by Next
+// we need dynamic from Next to be able to use then
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import { Avatar } from "antd";
+
+import "react-quill/dist/quill.snow.css";
 
 const CreatePostForm = ({ content, setContent, postSubmit }) => {
   return (
     <div className="card">
       <div className="card-body pb-4">
         <form className="form-group">
-          <textarea
+          <ReactQuill
+            theme="snow"
             value={content}
-            onChange={(e) => setContent(e.target.value)}
+            onChange={(e) => setContent(e)}
             placeholder="write something!"
             className="form-control"
-          ></textarea>
+          />
         </form>
       </div>
 
       <div className="card-footer">
-        <button onClick={postSubmit} className="btn btn-primary btn-sm mt-1">
+        <button
+          disabled={!content}
+          onClick={postSubmit}
+          className="btn btn-primary btn-sm mt-1"
+        >
           Post
         </button>
       </div>
